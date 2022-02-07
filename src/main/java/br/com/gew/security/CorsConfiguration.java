0d7c811c554eee476924f47cf75938239963f34a
ThiagoPetry <thiagokeiserpetry@gmail.com>
2022-02-07 13:26:49 -0300

    Teste para deploy

--- src/main/java/br/com/gew/security/CorsConfiguration.java
+++ src/main/java/br/com/gew/security/CorsConfiguration.java
@@ -14,7 +14,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://gew.software")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
