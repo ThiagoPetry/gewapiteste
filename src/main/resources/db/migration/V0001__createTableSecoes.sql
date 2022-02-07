4a8aca2cffc3f3915934f72209b08e55af85c296
ThiagoPetry <thiagokeiserpetry@gmail.com>
2022-02-07 10:06:20 -0300

    Teste para deploy

--- src/main/resources/db/migration/V0001__createTableSecoes.sql
+++ src/main/resources/db/migration/V0001__createTableSecoes.sql
@@ -1,28 +1,5 @@
 CREATE TABLE secoes(
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     responsavel_id BIGINT NOT NULL,
-    nome VARCHAR(255),
-
-    PRIMARY KEY(id)
+    nome VARCHAR(255) NOT NULL
 );
-
-DROP TRIGGER IF EXISTS `gew`.`secoes_BEFORE_INSERT`;
-
-DELIMITER $$
-USE `gew`$$
-CREATE DEFINER = CURRENT_USER TRIGGER `gew`.`secoes_BEFORE_INSERT` BEFORE INSERT ON `secoes` FOR EACH ROW
-BEGIN
-	SET @max := (SELECT MAX(id) FROM secoes);
-    SET @count := (SELECT COUNT(id) FROM secoes);
-
-    if (@max = @count) THEN
-		SET @next_value := @max + 1;
-    elseif (@count = 0) THEN
-        SET @next_value := 1;
-    else
-		SET @next_value := @count;
-    end if;
-
-    SET NEW.id = @next_value;
-END$$
-DELIMITER ;
