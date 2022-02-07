54f0d4b24624078ccfe34243eeb9be7a96d0b566
ThiagoPetry <thiagokeiserpetry@gmail.com>
2022-02-07 11:19:28 -0300

    Teste para deploy

--- src/main/resources/db/migration/V0016__createTableAtas.sql
+++ src/main/resources/db/migration/V0016__createTableAtas.sql
@@ -2,7 +2,7 @@ CREATE TABLE atas(
       id VARCHAR(255) NOT NULL PRIMARY KEY,
       nome VARCHAR(255) NOT NULL,
       tipo VARCHAR(255) NOT NULL,
-      data MEDIUMBLOB NOT NULL,
+      data BYTEA NOT NULL,
       criado_em timestamp DEFAULT current_timestamp,
       projeto_id BIGINT NOT NULL
 );
