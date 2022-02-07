cf74d43e360c1ed59509b8ab38ef486f921374cc
ThiagoPetry <thiagokeiserpetry@gmail.com>
2022-02-07 11:29:21 -0300

    Teste para deploy

--- src/main/resources/db/migration/V0019__insertSecoes.sql
+++ src/main/resources/db/migration/V0019__insertSecoes.sql
@@ -1,5 +1,5 @@
 INSERT INTO secoes(id, responsavel_id, nome) VALUES
-(null, 67266, "Departamento de E-commerce"),
-(null, 67279, "WEG Digitals"),
-(null, 67280, "Centro de Desenvolvimento"),
-(null, 67281, "Recursos Humanos");
+(null, 67266, 'Departamento de E-commerce'),
+(null, 67279, 'WEG Digitals'),
+(null, 67280, 'Centro de Desenvolvimento'),
+(null, 67281, 'Recursos Humanos');
--- src/main/resources/db/migration/V0021__insertCargos.sql
+++ src/main/resources/db/migration/V0021__insertCargos.sql
@@ -1,6 +1,6 @@
 INSERT INTO cargos(id, nome) VALUES
-(null, "ROLE_GERENTE"),
-(null, "ROLE_GESTOR"),
-(null, "ROLE_FORNECEDOR"),
-(null, "ROLE_CONSULTOR"),
-(null, "ROLE_FUNCIONARIO")
+(null, 'ROLE_GERENTE'),
+(null, 'ROLE_GESTOR'),
+(null, 'ROLE_FORNECEDOR'),
+(null, 'ROLE_CONSULTOR'),
+(null, 'ROLE_FUNCIONARIO')
