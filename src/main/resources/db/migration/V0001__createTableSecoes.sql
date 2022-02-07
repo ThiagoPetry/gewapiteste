0b95a5db21a6bcd67d71dfb7946e6ddddaa79591
ThiagoPetry <thiagokeiserpetry@gmail.com>
2022-02-07 10:52:38 -0300

    Teste para deploy

--- src/main/resources/db/migration/V0001__createTableSecoes.sql
+++ src/main/resources/db/migration/V0001__createTableSecoes.sql
@@ -4,3 +4,14 @@ CREATE TABLE secoes
     responsavel_id BIGINT NOT NULL,
     nome VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE funcionarios
+(
+    numero_cracha BIGINT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    cpf VARCHAR(12) NOT NULL,
+    telefone VARCHAR(14) NOT NULL,
+    valor_hora DOUBLE PRECISION NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+);
